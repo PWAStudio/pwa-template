ab3be03100e155e9353760f2761655a220033184
KILLER.CODES <Killercodes@users.noreply.github.com>
2022-09-01 10:44:41 +0530

    Updated with ReadeMe

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ if ("serviceWorker" in navigator) {
         .then(res => console.log("service worker registered"))
         .catch(err => console.log("service worker not registered", err))
     //})
-  }
+}
